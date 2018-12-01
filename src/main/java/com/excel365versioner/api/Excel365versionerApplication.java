622f6641c6bdd981d78b12258977f9c1528134bc
bowei <bowei.han100@gmail.com>
2018-12-01 13:09:37 -0500

    initial commit - spring initializr

---
+++ src/main/java/com/excel365versioner/api/Excel365versionerApplication.java
@@ -0,0 +1,12 @@
+package com.excel365versioner.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excel365versionerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Excel365versionerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/excel365versioner/api/excel365versioner/Excel365versionerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.excel365versioner.api.excel365versioner;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Excel365versionerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
