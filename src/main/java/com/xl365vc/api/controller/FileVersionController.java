3b88851bfc6cf50a874f26c13a7d0fec13dc6fbd
bowei <bowei.han100@gmail.com>
2018-12-01 15:26:03 -0500

    add endpoint for downloading versions

--- src/main/java/com/xl365vc/api/controller/FileVersionController.java
+++ src/main/java/com/xl365vc/api/controller/FileVersionController.java
@@ -1,10 +1,17 @@
 package com.xl365vc.api.controller;
 
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -47,7 +54,31 @@ public class FileVersionController {
         return new UploadFileResponse(fileName, fileDownloadUri,
                 file.getContentType(), file.getSize());
     }
-	
+
+    @GetMapping("/{name:.+}")
+    public ResponseEntity<Resource> getVersionByName(@PathVariable String name, HttpServletRequest request) {
+        // Load file as Resource
+        Resource resource = fileStorageService.loadFileAsResource(name);
+
+        // Try to determine file's content type
+        String contentType = null;
+        try {
+            contentType = request.getServletContext().getMimeType(resource.getFile().getAbsolutePath());
+        } catch (IOException ex) {
+            logger.info("Could not determine file type.");
+        }
+
+        // Fallback to the default content type if type could not be determined
+        if (contentType == null) {
+            contentType = "application/octet-stream";
+        }
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(contentType))
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
 	@DeleteMapping("/name/{fileName}")
 	public void removeVersion(@PathVariable("fileName") String fileName) {
 		fileStorageService.deleteFile(fileName);
